1750d8fbff38b1c01e74791fd0a5327054fcacdc
Eason0628 <zhaowintoo@gmail.com>
2026-06-18 10:34:32 +0800

    7.Install vue-i18n for handling multiple language switching

---
+++ src/common/locale/index.js
@@ -0,0 +1,20 @@
+import { createI18n } from "vue-i18n";
+import zh_CN from "./zh_CN";
+import en_US from "./en_US";
+
+const language = navigator.language || navigator.userLanguage;
+// let locale = language === 'zh-CN' ? 'zh_CN' : "en_US"
+let locale = "en_US"
+const i18n = createI18n({
+  legacy: false,
+  warnHtmlMessage: false,
+  globalInjection: true,
+  missingWarn: true,
+  locale,
+  messages: {
+    zh_CN,
+    en_US,
+  },
+});
+
+export default i18n;
--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import './styles/index.less'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './route/index.js'
-import {store} from './store/index.js'
+import { store } from './store/index.js'
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
- 
+import VueI18n from './common/locale/index.js'
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
 app.use(Antd);
+app.use(VueI18n);
 app.mount('#app');
